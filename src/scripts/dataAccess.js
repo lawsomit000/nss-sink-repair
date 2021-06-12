c6ad19b0a320917d8bea29b61384fe8262abae03
lawsomit000 <mitchell@mach33.tech>
2021-06-12 09:44:45 -0500

    first commit

---
+++ src/scripts/dataAccess.js
@@ -0,0 +1,16 @@
+const applicationState = {
+
+}
+
+const API = "http://localhost:8088"
+
+export const fetchRequests = () => {
+    return fetch(`${API}/requests`)
+        .then(response => response.json())
+        .then(
+            (serviceRequests) => {
+                // Store the external state in application state
+                applicationState.requests = serviceRequests
+            }
+        )
+}
