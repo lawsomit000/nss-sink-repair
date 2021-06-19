d73815439f3bb954069d603612c2a829051beb19
lawsomit000 <mitchell@mach33.tech>
2021-06-19 12:18:43 -0500

    final updates for this project. CSS still needs a little work

--- src/scripts/Request.js
+++ src/scripts/Request.js
@@ -1,14 +1,30 @@
 import { getRequests } from "./dataAccess.js"
+import {deleteRequest } from "./dataAccess.js"
 
 export const Requests = () => {
     const requests = getRequests()
     return `
-        <ul>
+        <ul class="allRequest">
             ${
                 requests.map(request => {
-                    return `<li>${request.description}</li>`
+                    return `
+                        <li class="listItem">${request.description}
+                            <button class="request__delete"
+                                    id="request--${request.id}">
+                                Delete
+                            </button>
+                        </li>`
                 }).join("")
             }
         </ul>
     `
 }
+
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [,requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
